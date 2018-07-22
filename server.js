ad2d0acb9d418f3660560285d1723b5be88f346e
Angristan <angristan@pm.me>
2018-07-22 16:33:01 +0200

    Add projects page

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'My projects',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request',
