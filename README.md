0bec9482fa57e7b4e03611397db31d546b5376ff
Angristan <angristan@pm.me>
2018-07-22 12:18:43 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# node-web-server
+
+A simple node web server to train with express and handlebars.
+
+Made thanks to this [Udemy course](https://www.udemy.com/the-complete-nodejs-developer-course-2/).
+
+## Installation
+
+```sh
+npm install
+```
+
+## Usage
+
+Start the server with:
+
+```sh
+node server.js
+```
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', `${log}\n`, (err) => {
+    if (err) {
+      console.log('Unable to append log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+hbs.registerHelper('screamIt', text => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome!',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
